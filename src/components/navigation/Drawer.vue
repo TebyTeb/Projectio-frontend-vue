5f27912337eafdf268384c3438117c1b0647ea03
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-03-21 00:14:47 +0000

    Logout funchion implemented

--- src/components/navigation/Drawer.vue
+++ src/components/navigation/Drawer.vue
@@ -1,14 +1,22 @@
 <script>
+import {useAuthStore} from '../../stores/authStore'
+
 export default {
   data: () => ({
+    store: useAuthStore(),
     drawer: null,
     links: [
-      ['mdi-inbox-arrow-down', 'Inbox', 'home'],
-      ['mdi-send', 'Send', 'about'],
-      ['mdi-delete', 'Trash', 'welcome'],
-      ['mdi-alert-octagon', 'Spam', 'wabout'],
+      ['mdi-inbox-arrow-down', 'Projects', 'home'],
+      ['mdi-send', 'Sprints', 'about'],
+      ['mdi-delete', 'Profile', 'workspace'],
     ],
-  })
+  }),
+  methods: {
+    logout() {
+      this.store.logout()
+      this.$router.push({name: 'login'})
+    }
+  }
 }
 </script>
 
@@ -17,7 +25,7 @@ export default {
     <v-sheet color="grey-lighten-1" class="pa-4">
       <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
 
-      <div>john@google.com</div>
+      <div>{{ store.getEmail }}</div>
     </v-sheet>
 
     <v-divider></v-divider>
@@ -31,6 +39,12 @@ export default {
         <router-link :to='{ name: route }'>
           <v-list-item-title>{{ text }}</v-list-item-title>
         </router-link>
+      </v-list-item>
+      <v-list-item @click="logout">
+        <template v-slot:prepend>
+          <v-icon>mdi-alert-octagon</v-icon>
+        </template>
+          <v-list-item-title>Logout</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
