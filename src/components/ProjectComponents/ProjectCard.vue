b5ace4dc0570770860f7e3e86d80a2f4e8f20769
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-04-05 21:51:25 +0100

    feat: :sparkles: User projects listed
    Now user projects are listed in the main workspace

--- src/components/ProjectComponents/ProjectCard.vue
+++ src/components/ProjectComponents/ProjectCard.vue
@@ -1,15 +1,32 @@
 <template>
-<v-card>
-  <v-card-title>
-    
-  </v-card-title>  
-</v-card>
+  <v-card class="d-flex flex-column ma-3" height="250px">
+    <v-card-item>
+      <v-card-title class="text-h4">
+        {{ project.title }}
+      </v-card-title>
+      <v-divider class="mt-2" />
+      <v-card-subtitle>
+        <p class="text-overline">Owner: <span class="text-body-2">{{ project.owner.name }}</span></p>
+      </v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+      <p class="text-subtitle-1">Description:</p>
+      <p class="text-body-1">{{ project.description }}</p>
+    </v-card-text>
+    <v-card-actions class="mb-0 justify-end">
+      <v-btn color="warning">Editar</v-btn>
+      <v-spacer />
+      <v-btn color="error">Eliminar</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-
+export default {
+  props: {
+    project: Object
+  }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,21 +1,39 @@
 <template>
-  <pre>
-    {{ projectList }}
-  </pre>
+  <v-row no-gutters>
+    <v-col
+      v-for="(project, idx) in projectList"
+      :key="idx"
+      cols="3"
+    >
+      <ProjectCard :project="project" />
+    </v-col>
+    <v-col cols="3">
+      <v-card class="d-flex ma-3" height="250px">
+        <v-card-text class="d-flex flex-column text-center align-center justify-center">
+          <v-icon icon="mdi-plus" size=50 class="ma-4" />
+          <span class="text-h6">CREAR NUEVO ROYECTO</span>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import API from '../Services/projectsService'
+import ProjectCard from '@/components/ProjectComponents/ProjectCard.vue'
 export default {
+  components: {
+    ProjectCard
+  },
   data() {
     return {
       projectList: []
     }
   },
-async beforeCreate() {
-  const projects = await API.getProjects()
-  this.projectList = projects
-}
+  async beforeCreate() {
+    const projects = await API.getProjects()
+    this.projectList = projects
+  }
 }
 </script>
 
