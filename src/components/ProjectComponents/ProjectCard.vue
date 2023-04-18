d7798098442504320d391b5a1f906bca9943916b
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-04-18 09:09:12 +0100

    feat: :sparkles: ProjectsView functionality in progress
    Implemented emits and handlers for create, edit and delete actions

--- src/components/ProjectComponents/ProjectCard.vue
+++ src/components/ProjectComponents/ProjectCard.vue
@@ -1,16 +1,30 @@
 <script setup>
+// IMPORTS //
+// -> Dependencies
+// -> Components
+import ProjectDialog from './ProjectDialog.vue'
+// Props //
 const props = defineProps(['project'])
+// Emits //
+const emit = defineEmits(['editProject', 'deleteProject'])
+// States //
+// Handlers //
+const goToProject = () => {
+  console.log(`Redirigiendo al proyecto ${props.project.title}`)
+}
 </script>
 
 <template>
-  <v-card class="d-flex flex-column ma-3" height="250px">
+  <v-card v-ripple.stop class="d-flex flex-column ma-3" height="250px" @click="goToProject">
     <v-card-item>
       <v-card-title class="text-h4">
         {{ project.title }}
       </v-card-title>
       <v-divider class="mt-2" />
       <v-card-subtitle>
-        <p class="text-overline">Owner: <span class="text-body-2">{{ props.project.owner.name }}</span></p>
+        <p class="text-overline">
+          Owner: <span class="text-body-2">{{ props.project.owner.name }}</span>
+        </p>
       </v-card-subtitle>
     </v-card-item>
     <v-card-text>
@@ -18,9 +32,12 @@ const props = defineProps(['project'])
       <p class="text-body-1">{{ props.project.description }}</p>
     </v-card-text>
     <v-card-actions class="mb-0 justify-end">
-      <v-btn color="warning">Editar</v-btn>
+      <v-btn color="warning">
+        Editar
+        <ProjectDialog btnTitle="edit" @editProject="emit('editProject')" />
+      </v-btn>
       <v-spacer />
-      <v-btn color="error">Eliminar</v-btn>
+      <v-btn color="error" @click.stop="emit('deleteProject')">Eliminar</v-btn>
     </v-card-actions>
   </v-card>
 </template>
