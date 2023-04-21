a8d7b1508c9ce74074275c554a8b60b53f547842
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-04-21 19:39:35 +0100

    feat: :sparkles: nav bar now handles login/logout dinamically

--- src/components/navigation/SystemBar.vue
+++ src/components/navigation/SystemBar.vue
@@ -1,16 +1,33 @@
-<!-- <script setup>
-</script> -->
+<script setup>
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore'
+const store = useAuthStore()
+const router = useRouter()
+
+const goToSignup = () => {
+  router.push({ name: 'signup' })
+}
+const goToLogin = () => {
+  router.push({ name: 'login' })
+}
+const logout = () => {
+  store.logout()
+  router.push({name: 'login'})
+}
+</script>
 
 <template>
   <v-app-bar color="primary" density="compact">
     <v-icon class="ml-2">mdi-dip-switch</v-icon>
     <v-app-bar-title>Projectio</v-app-bar-title>
     <v-spacer />
-    <v-icon>mdi-square</v-icon>
-
-    <v-icon>mdi-circle</v-icon>
-
-    <v-icon>mdi-triangle</v-icon>
+    <div v-if="store.isLoggedIn">
+      <v-btn variant="outlined" class="mx-2" @click="logout">logout</v-btn>
+    </div>
+    <div v-else>
+      <v-btn variant="outlined" @click="goToSignup">signup</v-btn>
+      <v-btn variant="outlined" class="mx-2" @click="goToLogin">login</v-btn>
+    </div>
   </v-app-bar>
 </template>
 
