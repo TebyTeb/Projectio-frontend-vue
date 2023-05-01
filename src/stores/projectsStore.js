d9aa5fc4caabd01c68dc4d99e456dc0c36062d7e
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-05-01 20:23:01 +0100

    fix: :bug: Removed annoying alerts

--- src/stores/projectsStore.js
+++ src/stores/projectsStore.js
@@ -14,21 +14,18 @@ export const useProjectStore = defineStore('project', () => {
     const response = await projectsAPI.createProject(projectData)
     if (response.error) return alert('Ups! Something went sideways...')
     await getProjects()
-    alert('Proyecto creado correctamente')
   }
   const editProject = async (id, projectData) => {
     console.log('Editando un proyecto desde el Dialog', projectData)
     const response = await projectsAPI.editProject(id, projectData)
     if (response.error) return alert('Ups! Something went sideways...')
     await getProjects()
-    alert('Proyecto editado correctamente')
   }
   const deleteProject = async (id) => {
     if (confirm('Do you really want to delete this project?')) {
       const response = await projectsAPI.deleteProject(id)
       if (response.error) return alert('Ups! Something went sideways...')
       await getProjects()
-      alert('Proyecto eliminado correctamente')
     }
   }
 
