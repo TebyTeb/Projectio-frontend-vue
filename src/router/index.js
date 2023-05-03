7609a687dddcf4aeecf3a00c57b556ff83d639d1
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-05-03 19:46:05 +0100

    fix: :bug: Deleted route without component view

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,6 @@ const routes = [
         component: () => import('../views/HomeView.vue')
       },
       {
-        path: 'about',
-        name: 'about',
-        component: () => import('@/views/AboutView.vue')
-      },
-      {
         path: '',
         name: 'login',
         component: () => import('@/views/LoginView.vue')
